55681a51d0fdb4c88ee1d231a585f0dd1cf14c0f
Manuel <manuel@guerrerocabrera.com>
2025-05-20 22:13:29 +0200

    checkSession

--- src/api/controllers/User.js
+++ src/api/controllers/User.js
@@ -153,7 +153,7 @@ const deleteUser = async (req, res, next) => {
 // Función que valida si un token es válido
 const checkSession = async (req, res, next) => {
     // return res.status(200).json({ user: req.user, token: req.headers.authorization });
-    return;
+    console.log("Hola");
 }
 
 // Función que recibe una dirección de correo electrónico del usuario y devuelve si es un email válido o no
